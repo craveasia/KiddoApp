6eb3c040b1f347ad2caabd6c5a5f675c1d604a72
Douglas Lee <douglas@craveasia.com>
2020-05-04 11:18:23 +0800

    Initial Updates.

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public string nextScene;
+    public Animator transition;
+    public float transitionTime = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            LoadNextScene();
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        StartCoroutine(LoadScene(nextScene));
+    }
+
+    IEnumerator LoadScene(string sceneName)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(nextScene);
+    }
+}
---
+++ Assets/Scripts/NextBtn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextBtn : MonoBehaviour
+{
+    public string nextScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void NextLevelButton()
+    {
+        // load the next scene
+        SceneManager.LoadScene(nextScene);
+    }
+}
