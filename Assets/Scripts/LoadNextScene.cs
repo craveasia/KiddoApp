d568aeca57c4e32928a5f4f7c88c9799ae61fd3d
GAMMAZONE\dougl <douglas128@gmail.com>
2020-05-08 00:38:28 +0800

    Added: 1. IntroLogo scene 2. Animated title scene. 3. Content Sponsor scene.
    Rename Intro scene to Welcome scene. Added a "Next" button into the ChildAge scene and linked all the scenes together with a new scene loader script (tempoprary).

---
+++ Assets/Scripts/LoadNextScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextScene : MonoBehaviour
+{
+    public string nextScene;
+    public Animator transition;
+    public float transitionTime = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadScene(nextScene));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+        IEnumerator LoadScene(string sceneName)
+    {
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(nextScene);
+    }
+}
