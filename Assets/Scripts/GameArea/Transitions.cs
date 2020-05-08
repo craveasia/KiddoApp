aa6b7997eb1d715a9d0eea9c9455c92ed09b0489
Douglas Lee <douglas@craveasia.com>
2020-05-08 18:05:35 +0800

    Merged Zaid's portion to the app.

---
+++ Assets/Scripts/GameArea/Transitions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Transitions : MonoBehaviour
+{
+    public Animator animation;
+    
+    // Update is called once per frame
+    public void ChildIcon()
+    {
+        animation.SetTrigger("Enter");
+    }
+
+    public void BackToMenu()
+    {
+        animation.SetTrigger("Exit");
+    }
+
+    public void LoadLevel(string scenename)
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void LoadLevels(int sceneno)
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGame(string scenename)
+    {
+        SceneManager.LoadScene("PlayArea");
+    }
+
+}
